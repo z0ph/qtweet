0ecaab2145fc9fcc9ef14248eb30d039ccf9dc83
Victor Grenu <victor.grenu@gmail.com>
2022-11-15 08:56:02 +0100

    using logging

--- python/handlers.py
+++ python/handlers.py
@@ -17,12 +17,12 @@ def lambda_handler(event, context):
     api = create_api()
     
     for record in event['Records']:
-        print(record['body'])
+        logging.info("record: " + record['body'])
         raw_record = record['body']
-        print("raw_length: ", len(raw_record))
+        logging.info("raw_length: " + len(raw_record))
         tweet = (raw_record[:275] + '..') if len(raw_record) > 279 else raw_record
-        print("tweet_length: ", len(tweet))
-        logging.info("[INFO] Publishing the queued tweet: " + tweet)
+        logging.info("tweet_length: " + len(tweet))
+        logging.info("Publishing the queued tweet: " + tweet)
         api.update_status(tweet)
 
     
@@ -35,4 +35,5 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
+    
     return response
