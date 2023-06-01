08616c629d6fab8b70c550f8169f3df5e1dbf2dc
Victor Grenu <victor@zoph.io>
2023-06-01 14:50:35 +0200

    update monitoring

--- tf/cloudwatch.tf
+++ tf/cloudwatch.tf
@@ -1,17 +1,18 @@
 resource "aws_cloudwatch_log_metric_filter" "error_log_filter" {
   name           = "${var.project}-error_log_filter"
-  pattern        = "[ERROR]"
+  pattern        = "ERROR"
   log_group_name = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
 
   metric_transformation {
     name      = "ErrorCount"
     namespace = "Custom/Lambda"
     value     = "1"
+    default_value = 0
   }
 }
 
 resource "aws_sns_topic" "notification_topic" {
-  name = "alert_topic_qtweet_mamip"
+  name = "${var.project}-alert_topic"
   tags = {
     Project     = "${var.project}"
     Environment = "${var.env}"
@@ -27,7 +28,7 @@ resource "aws_cloudwatch_metric_alarm" "error_log_filter_alarm" {
   namespace           = "Custom/Lambda"
   period              = "300"
   statistic           = "Sum"
-  treat_missing_data  = "ignore"
+  treat_missing_data  = "notBreaching"
   threshold           = "1"
   alarm_description   = "Alarm when the error log count exceeds 1"
   alarm_actions       = [aws_sns_topic.notification_topic.arn]
@@ -46,7 +47,8 @@ resource "aws_cloudwatch_metric_alarm" "sqs_alarm" {
   namespace           = "AWS/SQS"
   period              = "300"
   statistic           = "Sum"
-  threshold           = "2"
+  treat_missing_data  = "notBreaching"
+  threshold           = "1"
   alarm_description   = "Alarm when SQS messages visible exceeds 2 for 1 minute"
   alarm_actions       = [aws_sns_topic.notification_topic.arn]
   tags = {
